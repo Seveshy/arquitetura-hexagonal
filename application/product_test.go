cb40aebe897ad9399d84ca1fff1766bbd9385491
Daniel Major <daniel.alves@skytef.com.br>
2023-08-22 10:58:18 -0300

    Inicio do projeto com funcionalidades de produtos / 2 - testes

---
+++ application/product_test.go
@@ -0,0 +1,36 @@
+package application_test
+
+import (
+	"testing"
+
+	"github.com/codeedu/go-hexagonal/application"
+	"github.com/stretchr/testify/require"
+)
+
+func TestProduct_Enable(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	err := product.Enable()
+	require.Nil(t, err)
+
+	product.Price = 0
+	err = product.Enable()
+	require.Equal(t, "the price must be greater than zero to enable the product", err.Error())
+}
+
+func TestProduct_Disable(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Hello"
+	product.Status = application.DISABLED
+	product.Price = 0
+
+	err := product.Disable()
+	require.Nil(t, err)
+
+	product.Price = 10
+	err = product.Disable()
+	require.Equal(t, "the price must be zero in order to have the product disabled", err.Error())
+}
