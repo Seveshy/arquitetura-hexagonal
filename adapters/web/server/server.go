2e587adc4688745d3ac79a00c0ccd40e7a467422
Seveshy <danielalvesmajor@outlook.com>
2023-08-31 09:53:51 -0300

    Implementação de Handler / mocks

---
+++ adapters/web/server/server.go
@@ -0,0 +1,44 @@
+package server
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/codeedu/go-hexagonal/adapters/web/handler"
+	"github.com/codeedu/go-hexagonal/application"
+	"github.com/codegangsta/negroni"
+	"github.com/gorilla/mux"
+)
+
+type Webserver struct {
+	Service application.ProductServiceInterface
+}
+
+func MakeNewWebserver() *Webserver {
+	return &Webserver{}
+}
+
+func (w Webserver) Server() {
+
+	r := mux.NewRouter()
+	n := negroni.New(
+		negroni.NewLogger(),
+	)
+
+	handler.MakeProductHandler(r, n, w.Service)
+	http.Handle("/", r)
+
+	server := &http.Server{
+		ReadHeaderTimeout: 10 * time.Second,
+		WriteTimeout:      10 * time.Second,
+		Addr:              ":8080",
+		Handler:           http.DefaultServeMux,
+		ErrorLog:          log.New(os.Stderr, "log: ", log.Lshortfile),
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
--- cmd/cli.go
+++ cmd/cli.go
@@ -6,7 +6,7 @@ package cmd
 import (
 	"fmt"
 
-	"github.com/codeedu/go-hexagonal/adapters/db/cli"
+	"github.com/codeedu/go-hexagonal/adapters/cli"
 	"github.com/spf13/cobra"
 )
 
